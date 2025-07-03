155bbdddf2d35e1efcf0760e909f160101ef51b9
Shanmuhapriya <68014808+Shanmuhapriya@users.noreply.github.com>
2025-07-03 22:46:31 +0100

    bugs fixed

--- NeoWs-backend/index.js
+++ NeoWs-backend/index.js
@@ -1,21 +1,25 @@
-const express = require('express')
-const app = express() //creates an express application
-const port = 5000
-const cors = require('cors')
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
 
 require('dotenv').config();
 
 app.use(cors());
 
-app.get('/', (req, res) => { //app.get => defines get point
-  res.send('Hello World!')
-})
+// Serve static files from the React frontend build folder
+app.use(express.static(path.join(__dirname, 'path-to-frontend-build-folder')));
+
+// For all GET requests, send back React's index.html file.
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'path-to-frontend-build-folder', 'index.html'));
+});
+
+// Import router
+const neoRoutes = require('./routes/neo');
+app.use('/api/neo', neoRoutes);
 
 app.listen(port, () => {
-  console.log(`NeoWs-backend is running on port ${port}`)
-})
-
-//import router
-const neoRoutes = require('./routes/neo');
-//use router
-app.use('/api/neo', neoRoutes);
+  console.log(`NeoWs-backend is running on port ${port}`);
+});
